a49df3b5c2cb205f858c32b7002afb0f66aefdab
VictorGeruso <victor.geruso@gmail.com>
2019-07-17 17:23:41 -0300

    [UPD] Ajuste das propriedades e arescimo de comentários para melhoentendimento de cada model

--- src/models/Equipamento.js
+++ src/models/Equipamento.js
@@ -1,24 +1,52 @@
-const mongoose = require('mongoose')
+/**
+ * Este é o modelo para o armazenamento na collection de Equipamento no MongoDB.
+ * @author Victor Geruso
+ */
+const mongoose = require('mongoose') // Require da dependencia do mongoose
 
+// Inicindo o Schema para a criação do model
 const EquipamentoSchema = new mongoose.Schema({
+    /**
+     * Nome do item comprado
+     */
     item: {
         type: String
     },
+    /**
+     * Descrição do item para o jogador
+     */
     descricao: {
         type: String
     },
+    /**
+     * Bonos de defesa do item
+     * para itens de defesa
+     * 
+     * esta propriedade é usada para o calculo da classe de armadura
+     */
     bonusDefesa: {
         type: String
     },
+    /**
+     * Redução de movimento que o item pode causar no personagem
+     * para itens de defesa.
+     */
     reducaoMovimento: {
         type: Number
     },
+    /**
+     * preco do item para a compra do mesmo
+     */
     preco: {
         type: Number
     },
+    /**
+     * Peso do item em quilos(KG)
+     */
     peso: {
         type: Number
     }
 })
 
+// Criando o model
 mongoose.model('Equipamento', EquipamentoSchema)
--- src/models/Magia.js
+++ src/models/Magia.js
@@ -1,27 +1,41 @@
-const mongoose = require('mongoose')
+/**
+ * Este é o modelo para o armazenamento na collection de Magia no MongoDB.
+ * @author Victor Geruso
+ */
+const mongoose = require('mongoose') // Require da dependencia do mongoose
 
+// Inicindo o Schema para a criação do model
 const MagiaSchema = new mongoose.Schema({
+    /**
+     * O tipo da magia
+     * Existem dois tipos de magias:
+     * Arcanas e Divinas
+     * 
+     * Arcanas: geralmente usadas por personagem de classe mago.
+     * Divinas: Geralmente usadas por personagem de classe clerigo.
+     */
     tipo: {
         type: String
     },
-    tamanho: {
-        type: String
+    /**
+     * Especifica a duraçã da magia em minutos
+     */
+    duracao: {
+        type: Number
     },
+    /**
+     * O dano que a magia vai causar no oponente
+     */
     dano: {
-        type: String
+        type: Number
     },
+    /**
+     * O alcance da magia em raios os ou direção
+     */
     alcance: {
-        type: String
-    },
-    iniciativa: {
-        type: Number
-    },
-    preco: {
-        type: Number
-    },
-    peso: {
         type: Number
     }
 })
 
+// Criando o model
 mongoose.model('Magia', MagiaSchema)
